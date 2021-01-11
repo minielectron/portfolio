ecd20787f5bcd41a29d02f97fb49ee51833d677f
Prakash Sharma <prakash009Kp@gmail.com>
2021-01-11 09:30:29 +0530

    Add project model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    """
+    Represents a project model in home page.
+    """
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=150)
+    action_url = models.URLField(blank=True) 
+    image = models.ImageField( upload_to="website/images", max_length=None)
+
+    def __str__(self):
+        return self.title
